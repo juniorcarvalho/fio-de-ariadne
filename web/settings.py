f572a424ff0eb9ad4e30abf1d3009f2e30271a6f
Júnior Carvalho <joseadolfojr@gmail.com>
2020-04-28 10:00:07 -0300

    settings, alteração para f-string, Fix #6

--- web/settings.py
+++ web/settings.py
@@ -133,7 +133,7 @@ DEFAULT_FILE_STORAGE = "web.storage_backends.MediaStorage"
 AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", default="")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", default="")
 AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", default="")
-AWS_S3_CUSTOM_DOMAIN = f"%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
